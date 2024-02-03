f800ffe9b1d66b2ac85dd711aff0581a0f2f0401
jnicklr <jnichollaslr@gmail.com>
2024-02-03 17:05:58 -0300

    feat: Interface para o Repository.

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperNotes.Repositories
+{
+    public interface IRepository<T>
+    {
+        int Add(T obj);
+        int Update(T obj, int id);
+        int Delete(int id);
+        IEnumerable<T> Get();
+        IEnumerable<T> GetById(int id);
+    }
+}
