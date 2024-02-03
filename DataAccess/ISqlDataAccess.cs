fcd6aadf38874c23cca76c839d345d26d4256e81
jnicklr <jnichollaslr@gmail.com>
2024-02-03 16:58:02 -0300

    feat: SqlDataAccess.

---
+++ DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperNotes.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        IEnumerable<T> GetData<T, P>(string procedureName, P parameters, string connectionString);
+        int SaveData<T>(string procedureName, T parameters, string connectionString);
+    }
+}
---
+++ DataAccess/SqlDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperNotes.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        public IEnumerable<T> GetData<T, P>(string procedureName, P parameters, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                return connection.Query<T>(procedureName, parameters, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+        public int SaveData<T>(string procedureName, T parameters, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                return connection.Execute(procedureName, parameters, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+    }
+}
